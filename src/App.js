41a5bd8e3731f700f224bfc734535ce852646e5a
Jaianny Flores <101431446+jaiflores@users.noreply.github.com>
2022-07-13 09:11:28 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Prática React 03 - Component de function</h1>
+      <h2>Criando um projeto no CodeSanbox</h2>
+    </div>
+  );
+}
